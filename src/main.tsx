945bef8db129f768c20f828e2a23298d8a8464a7
Dipen Parmar <dipensavji@gmail.com>
2023-02-25 12:34:05 +0000

    Update 3 files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,26 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./main.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <div> Test Route </div>,
+  },
+]);
 
 const root = createRoot(document.getElementById("app"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// root.render(<App />);
